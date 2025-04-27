a2aa4bab0817f16ed2997c28aa6ff6b0fd6a147f
aminefaw89 <mohamed.amine.ameur1@gmail.com>
2025-04-27 16:58:08 -0400

    refactor: remove unused fs import in item test file

--- server/src/tests/integration/item.test.js
+++ server/src/tests/integration/item.test.js
@@ -6,7 +6,6 @@ import { loginFixture } from '../fixtures/login.fixture.js'
 import { enableDatabase } from '../setup.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import fs from 'fs'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
